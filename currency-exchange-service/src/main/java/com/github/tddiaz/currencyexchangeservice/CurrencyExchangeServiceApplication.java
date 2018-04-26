ae1b87458b1ddc996a83ee9f691d8b04326a4a43
tddiaz <tristantomdiaz@gmail.com>
2018-04-26 23:30:15 +0300

    setup currency-exchange-service

---
+++ currency-exchange-service/src/main/java/com/github/tddiaz/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.tddiaz.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+}
