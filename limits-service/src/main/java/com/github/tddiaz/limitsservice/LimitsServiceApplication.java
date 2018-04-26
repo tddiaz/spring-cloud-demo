3bdddbde6dc3810074895f054a9f2a9ad3f8d920
tddiaz <tristantomdiaz@gmail.com>
2018-04-26 22:25:53 +0300

    configured limits-service to fetch config properties from config server

--- limits-service/src/main/java/com/github/tddiaz/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/github/tddiaz/limitsservice/LimitsServiceApplication.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @SpringBootApplication
 @EnableConfigurationProperties
 public class LimitsServiceApplication {
