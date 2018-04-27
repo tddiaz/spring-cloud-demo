557b4fedbd81b513e80693f57f1e59c4cb3e4361
tddiaz <tristantomdiaz@gmail.com>
2018-04-28 01:12:51 +0300

    enable ribbon load balancing

--- currency-calculator-service/src/main/java/com/github/tddiaz/currencycalculatorservice/service/CurrencyExchangeIntegrationService.java
+++ currency-calculator-service/src/main/java/com/github/tddiaz/currencycalculatorservice/service/CurrencyExchangeIntegrationService.java
@@ -2,11 +2,13 @@ package com.github.tddiaz.currencycalculatorservice.service;
 
 
 import com.github.tddiaz.currencycalculatorservice.dto.CurrencyExchangeDto;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${currency-exchange-service.name}", url = "${currency-exchange-service.endpoint}")
+@FeignClient(name = "${currency-exchange-service.name}")
+@RibbonClient(name = "${currency-exchange-service.name}")
 public interface CurrencyExchangeIntegrationService {
 
     @GetMapping
