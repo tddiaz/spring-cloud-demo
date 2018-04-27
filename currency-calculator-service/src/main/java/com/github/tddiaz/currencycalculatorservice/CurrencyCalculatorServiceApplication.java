899e8ab3d5f535292a12e6957fb6be65acc580eb
tddiaz <tristantomdiaz@gmail.com>
2018-04-28 02:55:28 +0300

    register services to eureka

--- currency-calculator-service/src/main/java/com/github/tddiaz/currencycalculatorservice/CurrencyCalculatorServiceApplication.java
+++ currency-calculator-service/src/main/java/com/github/tddiaz/currencycalculatorservice/CurrencyCalculatorServiceApplication.java
@@ -2,11 +2,14 @@ package com.github.tddiaz.currencycalculatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(value = "com.github.tddiaz.currencycalculatorservice")
+@RefreshScope
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyCalculatorServiceApplication {
 
 	public static void main(String[] args) {
--- currency-exchange-service/src/main/java/com/github/tddiaz/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/github/tddiaz/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@RefreshScope
+
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
