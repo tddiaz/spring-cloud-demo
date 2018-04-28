e656fb13f2cc39e81f40378431cab271c544a376
tddiaz <tristantomdiaz@gmail.com>
2018-04-28 18:01:22 +0300

    setup zuul api gateway

---
+++ zuul-api-gateway/src/main/java/com/github/tddiaz/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.github.tddiaz.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@RefreshScope
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+}
