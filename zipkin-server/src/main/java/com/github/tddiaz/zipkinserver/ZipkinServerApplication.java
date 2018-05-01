84e2f87853770c50e025b37ba5afbcd99c44f9c2
tddiaz <tristantomdiaz@gmail.com>
2018-05-01 16:06:44 +0300

    configure zipkin server

---
+++ zipkin-server/src/main/java/com/github/tddiaz/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,18 @@
+package com.github.tddiaz.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
